50ecef06d850304c5678e03df2a7205c45f4fa66
milkyklim <klim.kolyvanov@gmail.com>
2019-07-08 17:22:00 +0200

    add notebook to check distibution of exons per plane

--- up-to-date/utils.py
+++ up-to-date/utils.py
@@ -14,6 +14,21 @@ def load_data(file_path, skiprows_=0):
         I = data[:, -1]
     return I
 
+# function for loading one data-set
+def load_i_z(file_path, skiprows_=0):
+    data = np.loadtxt(file_path, delimiter = '\t', skiprows=skiprows_)
+    # print(data.shape) 
+    # handles some weird cases, e.g. when there is no data in the file
+    if (len(data.shape) < 2):
+        data = data[None, :]
+    if (data.shape[1] == 0):
+        I = np.array([0])
+        z = np.array([0])
+    else:
+        I = data[:, -1]
+        z = data[:, -2]
+    return I, z
+
 # create title for the image
 def create_title(path, name_id=-1):
     # get the name of the initial image
